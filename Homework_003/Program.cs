9af7a164e9c2630923c057fbf75b3b26be9fe0e5
Valentina <valyapomortseva@gmail.com>
2023-02-26 16:11:40 +0700

    Решила задачу 3

--- Homework_003/Program.cs
+++ Homework_003/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 4;
+if (a%2 == 0) {
+    Console.WriteLine("да");
+}
+else {
+    Console.WriteLine("нет");
+}
