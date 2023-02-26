1a819553c169713921828e4753704b05e2a7f764
Valentina <valyapomortseva@gmail.com>
2023-02-26 18:40:58 +0700

    Написала алгоритм задачи 4

--- Homework_004/Program.cs
+++ Homework_004/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = 5;
+int index = 1;
+while (index < N) 
+{
+    index++;
+    Console.Write(index + " ");
+}
